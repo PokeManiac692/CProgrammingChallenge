b2543e09b2780d46c0d761a16ac71c82ea9f7760
PokeManiac692 <jeremyabuan@gmail.com>
2023-05-26 17:21:35 +0000

    Created linked list dictionary of all the words in user input sentence

--- .ccls-cache/@home@runner@Digitize/main.c
+++ .ccls-cache/@home@runner@Digitize/main.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_WORDS 100
+#define MAX_WORD_LENGTH 50
+
+/* Structure for dictionary entry */
+typedef struct Node {
+    char word[MAX_WORD_LENGTH];
+    int length;
+    char firstFour[5];
+    struct Node* next;
+} Node;
+
+Node dictionary[MAX_WORDS];  // Global memory for dictionary
+int dictionarySize = 0;  // Current size of the dictionary
 
 /* function to get a user input sentence */
 char* sentenceUserInput() {
@@ -8,12 +23,83 @@ char* sentenceUserInput() {
 
   printf("Type in a sentence: \n");
   fgets(sentenceInput, sizeof(sentenceInput), stdin);
-  printf("%s", sentenceInput);
+  // printf("%s", sentenceInput);
 
   return sentenceInput;
 }
 
+/* Function to add a word to the dictionary */
+void addToDictionary(char* word) {
+    if (dictionarySize >= MAX_WORDS) {
+        printf("Dictionary is full. Cannot add more words.\n");
+        return;
+    }
+
+    // Create a new node
+    Node* newNode = &dictionary[dictionarySize];
+
+    // Copy the word
+    strncpy(newNode->word, word, MAX_WORD_LENGTH);
+    newNode->word[MAX_WORD_LENGTH - 1] = '\0'; // Ensure null-terminated string
+  
+    // Store word length
+    newNode->length = strlen(word);
+
+    // Store the first four letters of the word
+    strncpy(newNode->firstFour, word, 4);
+    newNode->firstFour[4] = '\0'; // Ensure null-terminated string
+
+    newNode->next = NULL; // Set next pointer to NULL
+
+    // Link the new node to the previous node
+    if (dictionarySize > 0) {
+        dictionary[dictionarySize - 1].next = newNode;
+    }
+
+    dictionarySize++; // Increase the size of the dictionary
+}
+
+/* Function to create and return a linked list dictionary */
+Node* createDictionary(char* sentence) {
+    int sentenceLength = strlen(sentence);
+    int wordStart = 0;
+    int i;
+
+    for (i = 0; i <= sentenceLength; i++) {
+        if (sentence[i] == ' ' || sentence[i] == '\0') {
+            // Found a word boundary, extract the word
+            char word[MAX_WORD_LENGTH];
+            int wordLength = i - wordStart;
+            strncpy(word, &sentence[wordStart], wordLength);
+            word[wordLength] = '\0';
+
+            addToDictionary(word); // Add the word to the dictionary
+
+            wordStart = i + 1; // Update the word start index
+        }
+    }
+  
+    return &dictionary[0]; // Return the head of the linked list dictionary
+}
+
+/* Function to print the linked list dictionary */
+void printDictionary(Node* head) {
+    Node* current = head;
+
+    while (current != NULL) {
+        printf("Word: %s, Word length: %d, First four letters: %s\n", current->word, current->length, current->firstFour);
+        current = current->next;
+    }
+}
+
 int main(void) {
-  sentenceUserInput();
+  
+  char* sentence = sentenceUserInput();
+
+  Node* dictionary = createDictionary(sentence);
+
+  printf("Linked List Dictionary:\n");
+  printDictionary(dictionary);
+  
   return 0;
 }
